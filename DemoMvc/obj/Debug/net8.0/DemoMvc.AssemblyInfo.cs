498517232e5a08e3f0bba6f2bd6cce1e0fef0c3d
NTQ <quynhtan003@gmail.com>
2024-09-05 00:15:44 +0700

    commit note

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a6216f3d0c0bc0f1e165f65d15ca2b6ae7ac9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b2aa9d03d4d37b1387af8d2c8981ccb86785ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
